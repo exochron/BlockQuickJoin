e23006de76a63a8d1828904f00200d35897de7a8
exochron <exochron@freenet.de>
2020-12-06 20:20:47 +0100

    1.0.1 - fix reset during event update

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Block Quick Join
 
-Disables the party quick join notification.
+This addon disables the group notifications over the social button to join a friends or guildmates party.
+
+It does not affect the social button itself.
--- block.lua
+++ block.lua
@@ -1,6 +1,10 @@
+local function disable()
+    QUICK_JOIN_CONFIG.TOASTS_DISABLED = true
+end
+
+hooksecurefunc(C_SocialQueue, "GetConfig", disable)
+
 local frame = CreateFrame("Frame")
 frame:RegisterEvent("PLAYER_LOGIN")
 frame:RegisterEvent("SOCIAL_QUEUE_CONFIG_UPDATED")
-frame:SetScript("OnEvent", function()
-    QUICK_JOIN_CONFIG.TOASTS_DISABLED = true
-end)
+frame:SetScript("OnEvent", disable)
