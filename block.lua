5f50a22bb712f1e7d64d08a34ea4023c11feede3
exochron <exochron@freenet.de>
2020-12-06 20:39:52 +0100

    1.0.2 - fix hook >.<

--- block.lua
+++ block.lua
@@ -2,9 +2,10 @@ local function disable()
     QUICK_JOIN_CONFIG.TOASTS_DISABLED = true
 end
 
-hooksecurefunc(C_SocialQueue, "GetConfig", disable)
-
 local frame = CreateFrame("Frame")
 frame:RegisterEvent("PLAYER_LOGIN")
 frame:RegisterEvent("SOCIAL_QUEUE_CONFIG_UPDATED")
 frame:SetScript("OnEvent", disable)
+
+-- called during SOCIAL_QUEUE_CONFIG_UPDATED, but before check for CONFIG
+hooksecurefunc(QuickJoinToastButton, "ProcessQueuedUpdates", disable)
