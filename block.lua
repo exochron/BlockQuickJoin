d9bcef31ba14bf8da6e01fbb3e5e7127519dd70a
exochron <exochron@freenet.de>
2022-11-25 15:24:17 +0100

    1.0.4 - toc update - use EventRegistry

--- block.lua
+++ block.lua
@@ -1,11 +1,11 @@
+local ADDON_NAME = ...
+
 local function disable()
     QUICK_JOIN_CONFIG.TOASTS_DISABLED = true
 end
 
-local frame = CreateFrame("Frame")
-frame:RegisterEvent("PLAYER_LOGIN")
-frame:RegisterEvent("SOCIAL_QUEUE_CONFIG_UPDATED")
-frame:SetScript("OnEvent", disable)
+EventRegistry:RegisterFrameEventAndCallback("PLAYER_LOGIN", disable, ADDON_NAME)
+EventRegistry:RegisterFrameEventAndCallback("SOCIAL_QUEUE_CONFIG_UPDATED", disable, ADDON_NAME)
 
 -- called during SOCIAL_QUEUE_CONFIG_UPDATED, but before check for CONFIG
 hooksecurefunc(QuickJoinToastButton, "ProcessQueuedUpdates", disable)
